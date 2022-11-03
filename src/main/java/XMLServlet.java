86cb0f8c62ffabbd9b46b95a39d9cd6c8205a10c
Subibubbi2001 <99723591+Subibubbi2001@users.noreply.github.com>
2022-11-03 09:08:38 +0100

    Initial commit

---
+++ src/main/java/XMLServlet.java
@@ -0,0 +1,34 @@
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+
+@WebServlet(name = "XMLServlet", urlPatterns = "/api")
+public class XMLServlet extends HttpServlet {
+
+    protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Ahoy!");
+        writer.close();
+    }
+
+    protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String uid = req.getParameter("ID");
+        String pwd = req.getParameter("PW");
+
+        if (uid.toLowerCase().equals("hans") && pwd.equals("Secret") ) {
+            resp.addCookie(new Cookie("session", uid));
+            PrintWriter writer = resp.getWriter();
+            writer.print("Congratulations, "+uid+", you got in!");
+            writer.close();
+        } else {
+            resp.sendRedirect("index.html");
+        }
+    }
+
+}
